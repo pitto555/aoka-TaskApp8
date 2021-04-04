b252c2974662de6dba78272b3c7b702148351969
aoka <aoka.555.ibiza@gmail.com>
2021-04-04 22:51:47 +0900

    Initial Commit

---
+++ aoka-TaskApp8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aoka-TaskApp8
+//
+//  Created by aoka on 2021/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
