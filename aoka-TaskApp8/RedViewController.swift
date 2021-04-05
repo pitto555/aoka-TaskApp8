f41e9ce114001831bb746084649bdee8daa113d4
aoka <aoka.555.ibiza@gmail.com>
2021-04-05 22:08:24 +0900

    FirstCommit

---
+++ aoka-TaskApp8/RedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RedViewController.swift
+//  aoka-TaskApp8
+//
+//  Created by aoka on 2021/04/04.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    @IBOutlet private weak var calcLabel: UILabel!
+    
+    @IBOutlet private weak var slider: UISlider!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+      
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            
+            let sliderValue = delegate.count
+            
+            calcLabel.text = String(format: "%.6f", sliderValue)
+            slider.value = sliderValue
+        }
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            
+            let sliderValue = slider.value
+            
+            calcLabel.text = String(format: "%.6f", sliderValue)
+            delegate.count = sliderValue
+        }
+    }
+}
